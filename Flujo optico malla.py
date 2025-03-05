4df48137a79e6ad67a028b02e2d332c27af70d9f
Twisty93 <margun9742gmail.com>
2025-03-05 13:25:17 -0600

    Flujo optico y escalado

---
+++ Flujo optico malla.py
@@ -0,0 +1,69 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+lkparm = dict(winSize=(15,15), maxLevel=2,
+              criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+
+_, vframe = cap.read()
+vgris = cv.cvtColor(vframe, cv.COLOR_BGR2GRAY)
+
+p0 = np.array([(50,50), (100,50), (150,50), (200,50), (250,50),(300,50),
+               (50,100), (100,100), (150,100), (200,100), (250,100),(300,100),
+               (50,150), (100,150), (150,150), (200,150), (250,150),(300,150),
+               (50,200), (100,200), (150,200), (200,200), (250,200),(300,200),
+               (50,250), (100,250), (150,250), (200,250), (250,250),(300,250),
+               (50,300), (100,300), (150,300), (200,300), (250,300),(300,300),
+               (50,350), (100,350), (150,350), (200,350), (250,350),(300,350)
+
+               ])
+
+p0 = np.float32(p0[:, np.newaxis, :])
+
+mask = np.zeros_like(vframe)
+
+while True:
+    _, frame = cap.read()
+    fgris = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    p1, st, err = cv.calcOpticalFlowPyrLK(vgris, fgris, p0, None, **lkparm)
+
+    if p1 is None:
+        vgris = cv.cvtColor(vframe, cv.COLOR_BGR2GRAY)
+        mask = np.zeros_like(vframe)
+        cv.imshow('ventana', frame)
+    else:
+        bp1 = p1[st == 1]
+        bp0 = p0[st == 1]
+
+
+        num_cols = 5  
+
+        for i, (nv, vj) in enumerate(zip(bp1, bp0)):
+            a, b = (int(x) for x in nv.ravel())
+            c, d = (int(x) for x in vj.ravel())
+
+            frame = cv.circle(frame, (c, d), 2, (255, 0, 0), -1)  
+            frame = cv.circle(frame, (a, b), 3, (0, 255, 0), -1)  
+            
+           
+            if (i + 1) % num_cols != 0: 
+                next_idx = i + 1
+                if next_idx < len(bp1):
+                    x2, y2 = (int(x) for x in bp1[next_idx].ravel())
+                    frame = cv.line(frame, (a, b), (x2, y2), (0, 255, 255), 1)
+
+            
+            down_idx = i + num_cols
+            if down_idx < len(bp1): 
+                x3, y3 = (int(x) for x in bp1[down_idx].ravel())
+                frame = cv.line(frame, (a, b), (x3, y3), (255, 0, 255), 1)
+
+        cv.imshow('ventana', frame)
+        vgris = fgris.copy()
+
+        if (cv.waitKey(1) & 0xff) == 27:
+            break
+
+cap.release()
+cv.destroyAllWindows()
---
+++ escalar imagen y convolucion un noveno.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('1a.jpg', 0)
+import numpy as np
+x, y = img.shape
+
+
+scale_x, scale_y = 2, 2
+
+scaled_x, scaled_y = int(x * scale_x), int(y * scale_y)
+scaled_img = np.zeros((scaled_x, scaled_y), dtype=np.uint8)
+
+
+for i in range(x):
+    for j in range(y):
+     
+        scaled_img[i*2,j*2] = img[i, j]  
+
+cv.imshow('Imagen Escalada', scaled_img)
+
+kernel = np.ones((3, 3), np.float32) / 9
+
+
+convolved_img = np.zeros((scaled_x, scaled_y), dtype=np.uint8)
+
+
+for i in range(scaled_x):
+    for j in range(scaled_y):
+        suma = 0
+        for k in range(-1, 2):
+            for l in range(-1, 2):
+           
+                new_x = min(max(i + k, 0), scaled_x - 1)  
+                new_y = min(max(j + l, 0), scaled_y - 1)
+                suma += scaled_img[new_x, new_y] * kernel[k + 1, l + 1]
+
+  
+        convolved_img[i, j] = np.clip(suma, 0, 255)
+
+
+cv.imshow('Imagen Original', img)
+
+cv.imshow('Imagen Convolucionada', convolved_img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
